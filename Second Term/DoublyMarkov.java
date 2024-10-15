5c847649a370d63788632c77a4cfca3edbad7b36
idhantwastaken <idhantsrivastava024@gmail.com>
2024-10-15 20:27:44 +0530

    Second Term

--- Second Term/DoublyMarkov.java
+++ Second Term/DoublyMarkov.java
@@ -1,3 +1,42 @@
+import java.util.*;
 class DoublyMarkov {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the matrix");
+        int N = sc.nextInt();
+        while (N < 3 || N > 9) {
+            System.out.println("Size of the Matrix must be greater than 2 & less than 10. Try again");
+            N = sc.nextInt();
+        }
+        double M[][] = new double[N][N];
+        System.err.println("Enter " + (N * N) + " elements");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                M[i][j] = sc.nextDouble();
+                while (M[i][j] < 0) {
+                    System.out.println("Matrix elements must be positive. Try again");
+                    M[i][j] = sc.nextDouble();
+                }
+            }
+        }
+        
+        boolean isMarkov = false;
+        for (int i = 0; i < N; i++) {
+            int sum1 = 0; int sum2 = 0;
+            for (int j = 0; j < N; j++) {
+                sum1 += M[i][j];
+                sum2 += M[j][i];
+            }
+            if(sum1 != 1 || sum2 != 1){
+                System.out.println("The matrix is not Doubly Markov");
+                isMarkov = false;
+                break;
+            }
+            else isMarkov = true;
+        }
+        if (isMarkov) {
+            System.out.println("Matrix is Doubly Markov");
+        }
+        sc.close();
+    }
 }
--- Second Term/SaddlePoint.java
+++ Second Term/SaddlePoint.java
@@ -1,42 +1,33 @@
 import java.util.*;
 class SaddlePoint{
     public static void main(String args[]){
-        Scanner in=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("enter the size of the matrix");
-        int n=in.nextInt();
+        int n = sc.nextInt();
         System.out.println("enter the matrix");
-        int a[][]=new int[n][n];
-        for(int i=0;i<n;i++){
-            for(int p=0;p<n;p++){
-                a[i][p]=in.nextInt();
+        int a[][] = new int[n][n];
+        for(int i = 0; i < n; i++){
+            for(int p = 0; p < n; p++){
+                a[i][p] = sc.nextInt();
             }
         }
-        int min,max,saddle=0;
-        boolean contains_saddle=false;
-        for(int i=0;i<n;i++){
+        int min, max, saddle = 0;
+        boolean contains_saddle = false;
+        for(int i = 0; i < n; i++){
             for (int j = 0; j < a.length; j++) {
-                min=a[i][j];
-                max=a[i][j];
-                for(int p=0;p<n;p++){
-                    if(a[p][j]>max){
-                        max=a[p][j];
-                    }
-                    if(a[i][p]<min){
-                        min=a[i][p];
-                        
-                    }
+                min = a[i][j];
+                max = a[i][j];
+                for(int p = 0; p < n; p++){
+                    if(a[p][j] > max) max=a[p][j];
+                    if(a[i][p] < min) min=a[i][p]; 
                 }
-                if(min==max){
-                    saddle=min;
+                if(min == max){
+                    System.out.println("The sadle point is " + saddle);
                     contains_saddle=true;
                 }
             }
         }
-        if(contains_saddle){
-            System.out.println("The sadle point is "+saddle);
-        }
-        else
-        System.out.println("No sadle point");
-        in.close();
+        if(!contains_saddle) System.out.println("No sadle point");
+        sc.close();
     }
 }
