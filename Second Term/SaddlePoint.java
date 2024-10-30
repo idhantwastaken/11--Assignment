2249d79271abcf329266de3ee73b7c322f88cac8
idhantwastaken <idhantsrivastava024@gmail.com>
2024-10-30 21:31:33 +0530

    Second Term

--- Second Term/SaddlePoint.java
+++ Second Term/SaddlePoint.java
@@ -1,33 +1,42 @@
-import java.util.*;
+import java.util.Scanner;
 class SaddlePoint{
-    public static void main(String args[]){
+    int A[][]; int N;
+    void getArray(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the size of the matrix");
-        int n = sc.nextInt();
-        System.out.println("enter the matrix");
-        int a[][] = new int[n][n];
-        for(int i = 0; i < n; i++){
-            for(int p = 0; p < n; p++){
-                a[i][p] = sc.nextInt();
+        System.out.println("Enter the size of the matrix");
+        N = sc.nextInt();
+        System.out.println("Enter the matrix");
+        A = new int[N][N];
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                A[i][j] = sc.nextInt();
             }
         }
-        int min, max, saddle = 0;
+        sc.close();
+    }
+    
+    void findSaddlePoint(){
+        int min, max;
         boolean contains_saddle = false;
-        for(int i = 0; i < n; i++){
-            for (int j = 0; j < a.length; j++) {
-                min = a[i][j];
-                max = a[i][j];
-                for(int p = 0; p < n; p++){
-                    if(a[p][j] > max) max=a[p][j];
-                    if(a[i][p] < min) min=a[i][p]; 
+        for(int i = 0; i < N; i++){
+            for (int j = 0; j < N; j++) {
+                min = A[i][j];
+                max = A[i][j];
+                for(int p = 0; p < N; p++){
+                    if(A[p][j] > max) max=A[p][j];
+                    if(A[i][p] < min) min=A[i][p]; 
                 }
                 if(min == max){
-                    System.out.println("The sadle point is " + saddle);
+                    System.out.println("The sadle point is " + min);
                     contains_saddle=true;
                 }
             }
         }
         if(!contains_saddle) System.out.println("No sadle point");
-        sc.close();
+    }
+    public static void main(String args[]){
+        SaddlePoint ob = new SaddlePoint();
+        ob.getArray();
+        ob.findSaddlePoint();
     }
 }
